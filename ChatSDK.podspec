6285a91dfdeef7dc76008a8ffcdeebea1adc2048
nicktseng <nicktseng@gamania.com>
2025-06-26 08:49:02 +0800

    feat: update 1.0.38 xcframework

--- ChatSDK.podspec
+++ ChatSDK.podspec
@@ -1,13 +1,12 @@
-
 Pod::Spec.new do |s|
   s.name         = 'ChatSDK'
-  s.version      = '1.0.37'
+  s.version      = '1.0.38'
   s.summary      = 'Gamania Chat iOS Framework'
   s.description  = 'Messaging and Chat API for Mobile Apps'
   s.homepage     = 'https://github.com/gamania-beanfun/gim-chat-sdk-ios'
   s.license      = { :type => 'MIT', :file => 'LICENSE.md' }
   s.authors      = { 'Gamania' => 'g-raychen@gamania.com' }
-  s.source       = { :git => 'https://github.com/gamania-beanfun/gim-chat-sdk-ios.git', :tag => "1.0.37" }
+  s.source       = { :git => 'https://github.com/gamania-beanfun/gim-chat-sdk-ios.git', :tag => '1.0.38' }
   s.requires_arc = true
   s.platform     = :ios, '13.0'
   s.ios.vendored_frameworks = 'Sources/ChatSDK.xcframework'
--- Package.swift
+++ Package.swift
@@ -1,8 +1,7 @@
-
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
 let package = Package(
     name: "ChatSDK",
     platforms: [.iOS(.v13)],
@@ -16,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChatSDK",
-            url: "https://github.com/gamania-beanfun/gim-chat-sdk-ios/releases/download/1.0.37/ChatSDK.xcframework.zip",
-            checksum: "c30715ba0c40c479da662d139c1efba5360cfffb83b91d93a1e8c8b817cdd775"
+            url: "https://github.com/gamania-beanfun/gim-chat-sdk-ios/releases/download/1.0.38/ChatSDK.xcframework.zip",
+            checksum: "38b02cd47cc47115af0dfbfd760309dfeb7dccfe5ee946dbf58f31f579c78177"
         ),
     ]
 )
