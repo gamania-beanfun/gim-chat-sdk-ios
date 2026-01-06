e15ed024136cac3a1d66b381f88ba4e11de0621f
shuyusung <shuyusung@gamania.com>
2026-01-06 18:21:46 +0800

    chore: update version 1.7.0

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChatSDK",
-            url: "https://github.com/gamania-beanfun/gim-chat-sdk-ios/releases/download/1.6.1/ChatSDK.xcframework.zip",
-            checksum: "ea06bccc94da1358894eb4ab6c2a91a1a6da15e76c1a531e46990c77dc6b8532"
+            url: "https://github.com/gamania-beanfun/gim-chat-sdk-ios/releases/download/1.7.0/ChatSDK.xcframework.zip",
+            checksum: "e1ea1d3391dbe16351c2d4ecea232339cef1f22f5894ee83f073abc3ddcf9c14"
         )
     ]
 )
