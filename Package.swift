f71712cd0ead5737c731b5c7c086059c6936dce7
Shuyu SUNG(宋書瑜) <shuyusung@gamania.com>
2026-04-15 17:40:44 +0800

    release: 1.9.6

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChatSDK",
-            url: "https://github.com/gamania-beanfun/gim-chat-sdk-ios/releases/download/1.9.5/ChatSDK.xcframework.zip",
-            checksum: "ef7d044bac2ce5d8a643a857439ccd4be0749620b9d2c952944f84021c8bcf17"
+            url: "https://github.com/gamania-beanfun/gim-chat-sdk-ios/releases/download/1.9.6/ChatSDK.xcframework.zip",
+            checksum: "8e12c07bc658055b41b731475334bfe1935f423a5d0c0799d86b0ad6b14223b1"
         )
     ]
 )
